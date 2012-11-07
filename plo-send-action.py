43c9c06ebb232f0869c8765fda58896a0af81dd6
Jon Nordby <jononor@gmail.com>
2012-11-07 20:56:08 +0100

    Add a helper binary for sending application action messages over OSC

---
+++ plo-send-action.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Helper binary for sending an application action to the PLO server."""
+
+import liblo
+import sys, os
+
+if __name__ == '__main__':
+    if not len(sys.argv) == 3:
+        sys.stderr.write("Wrong number of arguments, expected 2.\n")
+        sys.stderr.write("Usage: %s APPLICATION ACTION-STRING\n" % sys.argv[0])
+        sys.exit(1)
+        
+    prog, application, action = sys.argv
+        
+    s = os.environ.get('PLO_SERVER', '127.0.0.1:1234')
+    host, port = s.split(':')
+    port = int(port)
+        
+    target = liblo.Address(host, port)
+    liblo.send(target, "/plo/player/action", application, action)
